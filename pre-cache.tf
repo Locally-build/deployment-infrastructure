aa4d3e327ee499d64ee1bf3a2ff2aba1a78d2c46
Carlos Lapao <cjlapao@gmail.com>
2023-12-05 13:17:38 +0000

    adding linux precache machine (#4)

--- pre-cache.tf
+++ pre-cache.tf
@@ -24,28 +24,28 @@ resource "parallels-desktop_remote_vm" "cache_linux_builder" {
   ]
 }
 
-# resource "parallels-desktop_remote_vm" "cache_macos_builder" {
-#   authenticator {
-#     api_key = parallels-desktop_auth.security.api_key[0].api_key
-#   }
+resource "parallels-desktop_remote_vm" "cache_macos_builder" {
+  authenticator {
+    api_key = parallels-desktop_auth.security.api_key[0].api_key
+  }
 
-#   host            = "${parallels-desktop_deploy.deploy[0].api.host}:${parallels-desktop_deploy.deploy[0].api.port}"
-#   name            = "macos14-github-actions-runner-base"
-#   owner           = "ec2-user"
-#   catalog_id      = "macos-14-github-actions-runner"
-#   version         = "v1"
-#   host_connection = "host=${var.catalog_username}:${var.catalog_password}@${var.catalog_host}"
-#   path            = "/Users/ec2-user/Parallels"
+  host            = "${parallels-desktop_deploy.deploy[0].api.host}:${parallels-desktop_deploy.deploy[0].api.port}"
+  name            = "macos14-github-actions-runner-base"
+  owner           = "ec2-user"
+  catalog_id      = "macos-14-github-actions-runner"
+  version         = "v1"
+  host_connection = "host=${var.catalog_username}:${var.catalog_password}@${var.catalog_host}"
+  path            = "/Users/ec2-user/Parallels"
 
-#   config {
-#     start_headless = true
-#   }
+  config {
+    start_headless = true
+  }
 
-#   force_changes = true
+  force_changes = true
 
-#   run_after_create = false
+  run_after_create = false
 
-#   depends_on = [
-#     parallels-desktop_deploy.deploy
-#   ]
-# }
+  depends_on = [
+    parallels-desktop_deploy.deploy
+  ]
+}
