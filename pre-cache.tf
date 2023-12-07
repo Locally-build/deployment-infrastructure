e04c9845fe3c92d5b2e1c6f0b6365abc18617a86
Carlos Lapao <cjlapao@gmail.com>
2023-12-07 15:09:20 +0000

    remove mac pre-cache (#8)

--- pre-cache.tf
+++ pre-cache.tf
@@ -25,30 +25,30 @@ resource "parallels-desktop_remote_vm" "cache_linux_builder" {
   ]
 }
 
-resource "parallels-desktop_remote_vm" "cache_macos_builder" {
-    count = var.host_count
-  host            = "${parallels-desktop_deploy.deploy[count.index].api.host}:${parallels-desktop_deploy.deploy[count.index].api.port}"
-  name            = "macos14-github-actions-runner-base"
-  owner           = "ec2-user"
-  catalog_id      = "macos-14-github-actions-runner"
-  version         = "v1"
-  host_connection = "host=${var.catalog_username}:${var.catalog_password}@${var.catalog_host}"
-  path            = "/Users/ec2-user/Parallels"
+# resource "parallels-desktop_remote_vm" "cache_macos_builder" {
+#     count = var.host_count
+#   host            = "${parallels-desktop_deploy.deploy[count.index].api.host}:${parallels-desktop_deploy.deploy[count.index].api.port}"
+#   name            = "macos14-github-actions-runner-base"
+#   owner           = "ec2-user"
+#   catalog_id      = "macos-14-github-actions-runner"
+#   version         = "v1"
+#   host_connection = "host=${var.catalog_username}:${var.catalog_password}@${var.catalog_host}"
+#   path            = "/Users/ec2-user/Parallels"
 
-  authenticator {
-    api_key = parallels-desktop_auth.security[count.index].api_key[0].api_key
-  }
+#   authenticator {
+#     api_key = parallels-desktop_auth.security[count.index].api_key[0].api_key
+#   }
 
 
-  config {
-    start_headless = true
-  }
+#   config {
+#     start_headless = true
+#   }
 
-  force_changes = true
+#   force_changes = true
 
-  run_after_create = false
+#   run_after_create = false
 
-  depends_on = [
-    parallels-desktop_deploy.deploy
-  ]
-}
+#   depends_on = [
+#     parallels-desktop_deploy.deploy
+#   ]
+# }
